da83ebf0307f27c73200eb9fc266b913b580c4c3
Andrew Davis <1709934+Savid@users.noreply.github.com>
2025-10-13 18:26:03 +1000

    refactor: combine api and frontend (#65)

--- frontend/src/utils/api-config.ts
+++ frontend/src/utils/api-config.ts
@@ -2,5 +2,5 @@ import type { CreateClientConfig } from '@api/client.gen';
 
 export const createClientConfig: CreateClientConfig = config => ({
   ...config,
-  baseUrl: import.meta.env.VITE_API_URL || 'http://localhost:8888/api/v1',
+  baseUrl: import.meta.env.VITE_API_URL || '/api/v1',
 });
